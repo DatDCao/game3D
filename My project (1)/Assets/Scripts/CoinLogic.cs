a016345ff43dd2244305ebe03df1a8d1d43c4972
DatdcHigame <docaodat432001@gmail.com>
2022-08-10 18:17:32 +0700

    ..

---
+++ My project (1)/Assets/Scripts/CoinLogic.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinLogic : MonoBehaviour
+{
+    AudioSource m_AudioSource;
+
+    Collider m_Colider;
+    MeshRenderer m_Meshreanderer;
+
+    [SerializeField]
+    AudioClip m_CoinSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+        m_Colider = GetComponent<Collider>();
+        m_Meshreanderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, .5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_AudioSource && m_CoinSound)
+        {
+            m_AudioSource.PlayOneShot(m_CoinSound);
+        }
+
+        if (m_Colider)
+        {
+            m_Colider.enabled = false;
+        }
+        if (m_Meshreanderer)
+        {
+            m_Meshreanderer.enabled = false;
+        }
+
+    }
+}
---
+++ My project (1)/Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    CharacterController m_charecterController;
+
+    float m_movementSpeed = 5f;
+
+    float m_horizontalInput, m_VerticalInput;
+
+    Vector3 m_movement;
+    Vector3 m_movementInput;
+
+    float m_jumpHeight = 0.5f;
+    float m_gravity = 0.2f;
+    bool m_jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_charecterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_VerticalInput = Input.GetAxis("Vertical");
+
+        m_movementInput = new Vector3(m_horizontalInput, 0, m_VerticalInput);
+
+        if (!m_jump && Input.GetButton("Jump"))
+        {
+            m_jump = true;
+        }
+        //Debug.Log("horizontal"+ m_horizontalInput);
+        //Debug.Log("vetical" + m_VerticalInput);
+    }
+
+    private void FixedUpdate()
+    {
+        m_movement = m_movementInput * m_movementSpeed * Time.deltaTime;
+
+        if(m_movementInput != Vector3.zero)
+        {
+            transform.forward = Quaternion.Euler(0, -90, 0) * m_movement.normalized;
+        }
+
+        // Apply gravity
+        if (!m_charecterController.isGrounded)
+        {
+            if (m_movement.y > 0)
+            {
+                m_movement.y -= m_gravity;
+            }
+            else
+            {
+                m_movement.y -= m_gravity;
+            }
+        }
+        else
+        {
+            m_movement.y = 0;
+        }
+        // Setting jumphight to movement y 
+        if (m_jump)
+        {
+            m_movement.y = m_jumpHeight;
+            m_jump = false;
+        }
+        if (m_charecterController)
+        {
+            m_charecterController.Move(m_movement);
+        }
+    }
+}
